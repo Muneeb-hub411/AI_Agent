385914714f59f8deb0f34ed9117acf3e70e0ca64
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-04-08 11:32:05 +0500

    - we have to change openAI api bcz its paid ☠️

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ dotenv.config();
 const client = new OpenAI({
   apiKey: process.env.Open_AI_API_KEY,
 });
+
+const response = await client.responses.create({
+  model: "gpt-5",
+  instructions: "You are a coding assistant that talks like a pirate",
+  input: "Are semicolons optional in JavaScript?",
+});
+
+console.log(response.output_text);
